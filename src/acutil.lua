a5f07f960e43f9959bf670e8f2582ba9f5a39a91
Mizuki <MizukiBelhi@users.noreply.github.com>
2021-04-27 04:57:21 -0700

    Update acutil.lua

--- src/acutil.lua
+++ src/acutil.lua
@@ -1,7 +1,7 @@
 local acutil = {};
 local json = require('json');
 local addonSavePath = '../addons/';
-local depreciationList = {};
+depreciationList = {};
 
 _G['ADDONS'] = _G['ADDONS'] or {};
 _G['ADDONS']['EVENTS'] = _G['ADDONS']['EVENTS'] or {};
@@ -53,19 +53,19 @@ function acutil.rightPad(str, len, char)
 end
 
 function acutil.tostring(var) 
-    if (var == nil) then return 'nil'; end  
-    local tp = type(var); 
-    if (tp == 'string' or tp == 'number') then 
-        return var; 
-    end
-    if (tp == 'boolean') then 
-        if (var) then 
-            return 'true';
-        else
-            return 'false';
-        end
-    end
-    return tp;
+	if (var == nil) then return 'nil'; end	
+	local tp = type(var); 
+	if (tp == 'string' or tp == 'number') then 
+		return var; 
+	end
+	if (tp == 'boolean') then 
+		if (var) then 
+			return 'true';
+		else
+			return 'false';
+		end
+	end
+	return tp;
 end
 
 -- ================================================================
@@ -73,135 +73,135 @@ end
 -- ================================================================
 
 function acutil.getStatPropertyFromPC(typeStr, statStr, pc)
-    local errorText = "Param was nil";
+	local errorText = "Param was nil";
 
-    if typeStr ~= nil and statStr ~= nil and pc ~= nil then
+	if typeStr ~= nil and statStr ~= nil and pc ~= nil then
 
-        if typeStr == "JOB" then
-            if statStr == "STR" then
-                return pc.STR_JOB;
-            elseif statStr == "DEX" then
-                return pc.DEX_JOB;
-            elseif statStr == "CON" then
-                return pc.CON_JOB;
-            elseif statStr == "INT" then
-                return pc.INT_JOB;
-            elseif statStr == "MNA" then
-                return pc.MNA_JOB;
-            elseif statStr == "LUCK" then
-                return pc.LUCK_JOB;
-            else
-                errorText = "Could not find stat "..statStr.." for type "..typeStr;
-            end
+		if typeStr == "JOB" then
+			if statStr == "STR" then
+				return pc.STR_JOB;
+			elseif statStr == "DEX" then
+				return pc.DEX_JOB;
+			elseif statStr == "CON" then
+				return pc.CON_JOB;
+			elseif statStr == "INT" then
+				return pc.INT_JOB;
+			elseif statStr == "MNA" then
+				return pc.MNA_JOB;
+			elseif statStr == "LUCK" then
+				return pc.LUCK_JOB;
+			else
+				errorText = "Could not find stat "..statStr.." for type "..typeStr;
+			end
 
-        elseif typeStr == "STAT" then
-            if statStr == "STR" then
-                return pc.STR_STAT;
-            elseif statStr == "DEX" then
-                return pc.DEX_STAT;
-            elseif statStr == "CON" then
-                return pc.CON_STAT;
-            elseif statStr == "INT" then
-                return pc.INT_STAT;
-            elseif statStr == "MNA" then
-                return pc.MNA_STAT;
-            elseif statStr == "LUCK" then
-                return pc.LUCK_STAT;
-            else
-                errorText = "Could not find stat "..statStr.." for type "..typeStr;
-            end
+		elseif typeStr == "STAT" then
+			if statStr == "STR" then
+				return pc.STR_STAT;
+			elseif statStr == "DEX" then
+				return pc.DEX_STAT;
+			elseif statStr == "CON" then
+				return pc.CON_STAT;
+			elseif statStr == "INT" then
+				return pc.INT_STAT;
+			elseif statStr == "MNA" then
+				return pc.MNA_STAT;
+			elseif statStr == "LUCK" then
+				return pc.LUCK_STAT;
+			else
+				errorText = "Could not find stat "..statStr.." for type "..typeStr;
+			end
 
-        elseif typeStr == "BONUS" then
-            if statStr == "STR" then
-                return pc.STR_Bonus;
-            elseif statStr == "DEX" then
-                return pc.DEX_Bonus;
-            elseif statStr == "CON" then
-                return pc.CON_Bonus;
-            elseif statStr == "INT" then
-                return pc.INT_Bonus;
-            elseif statStr == "MNA" then
-                return pc.MNA_Bonus;
-            else
-                errorText = "Could not find stat "..statStr.." for type "..typeStr;
-            end
+		elseif typeStr == "BONUS" then
+			if statStr == "STR" then
+				return pc.STR_Bonus;
+			elseif statStr == "DEX" then
+				return pc.DEX_Bonus;
+			elseif statStr == "CON" then
+				return pc.CON_Bonus;
+			elseif statStr == "INT" then
+				return pc.INT_Bonus;
+			elseif statStr == "MNA" then
+				return pc.MNA_Bonus;
+			else
+				errorText = "Could not find stat "..statStr.." for type "..typeStr;
+			end
 
-        elseif typeStr == "ADD" then
-            if statStr == "STR" then
-                return pc.STR_ADD;
-            elseif statStr == "DEX" then
-                return pc.DEX_ADD;
-            elseif statStr == "CON" then
-                return pc.CON_ADD;
-            elseif statStr == "INT" then
-                return pc.INT_ADD;
-            elseif statStr == "MNA" then
-                return pc.MNA_ADD;
-            elseif statStr == "LUCK" then
-                return pc.LUCK_ADD;
-            else
-                errorText = "Could not find stat "..statStr.." for type "..typeStr;
-            end
+		elseif typeStr == "ADD" then
+			if statStr == "STR" then
+				return pc.STR_ADD;
+			elseif statStr == "DEX" then
+				return pc.DEX_ADD;
+			elseif statStr == "CON" then
+				return pc.CON_ADD;
+			elseif statStr == "INT" then
+				return pc.INT_ADD;
+			elseif statStr == "MNA" then
+				return pc.MNA_ADD;
+			elseif statStr == "LUCK" then
+				return pc.LUCK_ADD;
+			else
+				errorText = "Could not find stat "..statStr.." for type "..typeStr;
+			end
 
-        elseif typeStr == "BM" then
-            if statStr == "STR" then
-                return pc.STR_BM;
-            elseif statStr == "DEX" then
-                return pc.DEX_BM;
-            elseif statStr == "CON" then
-                return pc.CON_BM;
-            elseif statStr == "INT" then
-                return pc.INT_BM;
-            elseif statStr == "MNA" then
-                return pc.MNA_BM;
-            elseif statStr == "LUCK" then
-                return pc.LUCK_BM;
-            else
-                errorText = "Could not find stat "..statStr.." for type "..typeStr;
-            end
+		elseif typeStr == "BM" then
+			if statStr == "STR" then
+				return pc.STR_BM;
+			elseif statStr == "DEX" then
+				return pc.DEX_BM;
+			elseif statStr == "CON" then
+				return pc.CON_BM;
+			elseif statStr == "INT" then
+				return pc.INT_BM;
+			elseif statStr == "MNA" then
+				return pc.MNA_BM;
+			elseif statStr == "LUCK" then
+				return pc.LUCK_BM;
+			else
+				errorText = "Could not find stat "..statStr.." for type "..typeStr;
+			end
 
-        else
-            errorText = "Could not find a property for type "..typeStr;
-        end
-    end
+		else
+			errorText = "Could not find a property for type "..typeStr;
+		end
+	end
 
-    ui.SysMsg(errorText);
-    return 0;
+	ui.SysMsg(errorText);
+	return 0;
 end
 
 function acutil.isValidStat(statStr, includeLuck)
-    if statStr == "LUCK" then
-        return includeLuck;
-    elseif statStr == "STR" or
-           statStr == "DEX" or
-           statStr == "CON" or
-           statStr == "INT" or
-           statStr == "MNA" then
-        return true;
-    end
+	if statStr == "LUCK" then
+		return includeLuck;
+	elseif statStr == "STR" or
+		   statStr == "DEX" or
+		   statStr == "CON" or
+		   statStr == "INT" or
+		   statStr == "MNA" then
+		return true;
+	end
 
-    return false;
+	return false;
 end
 
 function acutil.textControlFactory(attributeName, isMainSection)
-    local text = "";
+	local text = "";
 
-    if attributeName == "MNA" then
-        attributeName = "SPR"
-    elseif attributeName == "MountDEF" then
-        attributeName = "physical defense"
-    elseif attributeName == "MountDR" then
-        attributeName = "evasion"
-    elseif attributeName == "MountMHP" then
-        attributeName = "max HP"
-    end
+	if attributeName == "MNA" then
+		attributeName = "SPR"
+	elseif attributeName == "MountDEF" then
+		attributeName = "physical defense"
+	elseif attributeName == "MountDR" then
+		attributeName = "evasion"
+	elseif attributeName == "MountMHP" then
+		attributeName = "max HP"
+	end
 
-    if isMainSection then
-        text = "Points invested in " .. attributeName;
-    else
-        text = "Mounted " .. attributeName .. " bonus";
-    end
-    return text;
+	if isMainSection then
+		text = "Points invested in " .. attributeName;
+	else
+		text = "Mounted " .. attributeName .. " bonus";
+	end
+	return text;
 end
 
 -- ================================================================
@@ -209,26 +209,26 @@ end
 -- ================================================================
 
 function acutil.getItemRarityColor(itemObj)
-    local itemProp = geItemTable.GetProp(itemObj.ClassID);
-    local grade = itemObj.ItemGrade;
+	local itemProp = geItemTable.GetProp(itemObj.ClassID);
+	local grade = itemObj.ItemGrade;
 
-    if (itemObj.ItemType == "Recipe") then
-        local recipeGrade = string.match(itemObj.Icon, "misc(%d)");
-        if recipeGrade ~= nil then
-            grade = tonumber(recipeGrade) - 1
-            grade = (grade > 1 and grade) or 1
-        end
-    end
-    
-    if (itemProp.setInfo ~= nil) then return "00FF00"; -- set piece 
-    elseif (grade == 0) then return "FFBA03"; -- premium
-    elseif (grade == 1) then return "FFFFFF"; -- common
-    elseif (grade == 2) then return "108CFF"; -- rare
-    elseif (grade == 3) then return "AA40FF"; -- epic
-    elseif (grade == 4) then return "FF4F00"; -- Good old Red-ish Legendary that degraded to unique
-    elseif (grade == 5) then return "EFEA00"; -- All new Bright-Yellow legendary. (Solmiki/Lolo/Some event item. might useful in future)
-    end 
-    return "E1E1E1"; -- no grade (non-equipment items)
+	if (itemObj.ItemType == "Recipe") then
+		local recipeGrade = string.match(itemObj.Icon, "misc(%d)");
+		if recipeGrade ~= nil then
+			grade = tonumber(recipeGrade) - 1
+			grade = (grade > 1 and grade) or 1
+		end
+	end
+	
+	if (itemProp.setInfo ~= nil) then return "00FF00"; -- set piece 
+	elseif (grade == 0) then return "FFBA03"; -- premium
+	elseif (grade == 1) then return "FFFFFF"; -- common
+	elseif (grade == 2) then return "108CFF"; -- rare
+	elseif (grade == 3) then return "AA40FF"; -- epic
+	elseif (grade == 4) then return "FF4F00"; -- Good old Red-ish Legendary that degraded to unique
+	elseif (grade == 5) then return "EFEA00"; -- All new Bright-Yellow legendary. (Solmiki/Lolo/Some event item. might useful in future)
+	end 
+	return "E1E1E1"; -- no grade (non-equipment items)
 end
 
 -- ================================================================
@@ -269,7 +269,7 @@ end
 
 -- usage:
 -- function myFunc(addonFrame, eventMsg)
---     local arg1, arg2, arg3 = acutils.getEventArgs(eventMsg);
+--	   local arg1, arg2, arg3 = acutils.getEventArgs(eventMsg);
 -- end
 function acutil.getEventArgs(eventMsg)
 	return unpack(_G['ADDONS']['EVENTS']['ARGS'][eventMsg]);
@@ -287,9 +287,11 @@ function acutil.saveJSON(path, tbl)
 	file:write(s);
 	file:close();
 	
-	if depreciationList[path] == nil then
+	local np = string.sub(path, 3);
+	np = np:gsub('%/', '');
+	if depreciationList[np] == nil then
+		depreciationList[np] = true;
 		print("acutil.saveJSON has been depreciated, please use acutil.saveJSONX('myaddon/data.json', {somedata}) instead. Path: "..path);
-		depreciationList[path] = true;
 	end
 	
 end
@@ -334,9 +336,10 @@ function acutil.loadJSON(path, tblMerge, ignoreError)
 		acutil.saveJSON(path, t);
 	end
 	-- returning the table
-	if depreciationList[path] == nil then
+	np = np:gsub('%/', '');
+	if depreciationList[np] == nil then
+		depreciationList[np] = true;
 		print("acutil.loadJSON has been depreciated, please use acutil.loadJSONX('myaddon/data.json') instead. Path: "..path);
-		depreciationList[path] = true;
 	end
 	return t;
 end
@@ -391,17 +394,17 @@ end
 
 -- table length (when #table doesn't works)
 function acutil.tableLength(T)
-    local count = 0
-    for _ in pairs(T) do count = count + 1 end
-    return count
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
 end
 
 -- ================================================================
 -- Logging
 -- ================================================================
 
-function acutil.log(msg)     
-    CHAT_SYSTEM(acutil.tostring(msg));  
+function acutil.log(msg)	 
+	CHAT_SYSTEM(acutil.tostring(msg));	
 end
 
 -- ================================================================
@@ -417,75 +420,75 @@ function acutil.slashCommand(cmd, fn)
 end
 
 function acutil.slashSet(set)
-    if (not set.base) then return ui.SysMsg('[acutil.slashSet] missing "base" string.'); end
-    if (set.title) then set.title = set.title..'{nl}-----------{nl}'; else set.title = ''; end
+	if (not set.base) then return ui.SysMsg('[acutil.slashSet] missing "base" string.'); end
+	if (set.title) then set.title = set.title..'{nl}-----------{nl}'; else set.title = ''; end
 
-    local fnError = set.error;
+	local fnError = set.error;
 
-    if (not fnError) then
-        fnError = function(extraMsg)
-            if (extraMsg) then extraMsg = '{nl}-----------{nl}'..extraMsg..'{nl}-----------{nl}'; else extraMsg = ''; end
-            return 'Command not valid.{nl}'..extraMsg..'Type "'..set.base..'" for help.', '', 'Nope';
-        end
-    end
+	if (not fnError) then
+		fnError = function(extraMsg)
+			if (extraMsg) then extraMsg = '{nl}-----------{nl}'..extraMsg..'{nl}-----------{nl}'; else extraMsg = ''; end
+			return 'Command not valid.{nl}'..extraMsg..'Type "'..set.base..'" for help.', '', 'Nope';
+		end
+	end
 
-    local executeSetCmd = function(fn,params) 
-        local p1, p2, p3;
-        if (params) then
-            p1,p2,p3 = fn(params);
-        else
-            p1,p2,p3 = fn(); 
-        end         
-        if (p1) then 
-            local msg = set.title..p1;
-            if (p2 and p3) then return ui.MsgBox(msg,p2,p3); end
-            if (p2) then return ui.MsgBox(msg,p2); end
-            return ui.MsgBox(msg);
-        end
-    end
+	local executeSetCmd = function(fn,params) 
+		local p1, p2, p3;
+		if (params) then
+			p1,p2,p3 = fn(params);
+		else
+			p1,p2,p3 = fn(); 
+		end			
+		if (p1) then 
+			local msg = set.title..p1;
+			if (p2 and p3) then return ui.MsgBox(msg,p2,p3); end
+			if (p2) then return ui.MsgBox(msg,p2); end
+			return ui.MsgBox(msg);
+		end
+	end
 
-    local mainFn = function(words)
-        local word = table.remove(words,1);     
-        if (word == 'help') then word = nil; end
+	local mainFn = function(words)
+		local word = table.remove(words,1);		
+		if (word == 'help') then word = nil; end
 
-        if (word) then
-            for cmd,data in pairs(set.cmds) do
-                if (cmd == word) then
-                    local fn = data.fn;     
-                    local qtexpected = data.nparams or 0;                   
-                    local qtfound = acutil.tableLength(words);
-                    if (qtfound ~= qtexpected) then
-                        return executeSetCmd(fnError,set.base..' '..cmd..' expects '..qtexpected..' params, not '..qtfound..'.');
-                    else
-                        local params = {}; 
-                        local n = 0;
-                        while (acutil.tableLength(words) > 0) do
-                            params[n] = table.remove(words,1);
-                            n = n+1;
-                        end
-                        return executeSetCmd(fn,params);
-                    end
-                end
-            end
-            return executeSetCmd(fnError,word..' is not a valid call.');
-        else
-            if (set.empty) then
-                return executeSetCmd(set.empty);
-            end
-            local lines = set.base..'{nl}Show addon help.{nl}-----------{nl}';
-            for cmd,data in pairs(set.cmds) do
-                local params = ' ';
-                local qtparams = data.nparams or 0;
-                for i = 1, qtparams do 
-                    params = params .. '$param'..i..' '; 
-                end
-                lines = lines .. set.base..' '..cmd..params..'{nl}-----------{nl}';
-            end
-            return ui.MsgBox(set.title..lines,'','Nope');
-        end
-    end
+		if (word) then
+			for cmd,data in pairs(set.cmds) do
+				if (cmd == word) then
+					local fn = data.fn;		
+					local qtexpected = data.nparams or 0;					
+					local qtfound = acutil.tableLength(words);
+					if (qtfound ~= qtexpected) then
+						return executeSetCmd(fnError,set.base..' '..cmd..' expects '..qtexpected..' params, not '..qtfound..'.');
+					else
+						local params = {}; 
+						local n = 0;
+						while (acutil.tableLength(words) > 0) do
+							params[n] = table.remove(words,1);
+							n = n+1;
+						end
+						return executeSetCmd(fn,params);
+					end
+				end
+			end
+			return executeSetCmd(fnError,word..' is not a valid call.');
+		else
+			if (set.empty) then
+				return executeSetCmd(set.empty);
+			end
+			local lines = set.base..'{nl}Show addon help.{nl}-----------{nl}';
+			for cmd,data in pairs(set.cmds) do
+				local params = ' ';
+				local qtparams = data.nparams or 0;
+				for i = 1, qtparams do 
+					params = params .. '$param'..i..' '; 
+				end
+				lines = lines .. set.base..' '..cmd..params..'{nl}-----------{nl}';
+			end
+			return ui.MsgBox(set.title..lines,'','Nope');
+		end
+	end
    
-    acutil.slashCommand(set.base,mainFn);
+	acutil.slashCommand(set.base,mainFn);
 end
 
 function acutil.onUIChat(msg)
